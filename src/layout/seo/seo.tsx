e2a9134961c1fdb683f8ea319522a7292f6f3810
NYN1100 <yusufnimatullayev@gmail.com>
2023-09-10 17:58:57 +0500

    second commit

--- src/layout/seo/seo.tsx
+++ src/layout/seo/seo.tsx
@@ -11,7 +11,7 @@ const SEO = ({
   return (
     <>
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{metaTitle}</title>
 
